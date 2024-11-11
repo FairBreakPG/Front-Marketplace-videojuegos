508aecc4c7102aa5f7a6a2e896f1b84f3ff9b6f4
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-11-11 01:13:52 -0600

    Se eliminan imports no usados

--- src/components/NavbarSuperior/Navbar.jsx
+++ src/components/NavbarSuperior/Navbar.jsx
@@ -1,10 +1,7 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { formatoNumero } from "../../formatoNumero.js";
-import { PizzasContext } from "../../context/PizzaProvider.jsx";
 
 const Navbar = () => {
-  const { total } = useContext(PizzasContext);
   const navegar = useNavigate(); 
   const loginClikc = () => navegar("/login");
   const registroClick = () => navegar("/registrar-usuario");
@@ -47,7 +44,6 @@ const Navbar = () => {
               </button>
               <button onClick={() => navegar("/carrito")} className="btn btn-light d-flex align-items-center">
                 <span>&#128722;</span> 
-                <span className="ms-2">${formatoNumero(total)}</span>
               </button>
             </div>
           </div>
