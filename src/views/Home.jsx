2c85986fed658cb12fd1507b22229bb07fc26ef6
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-11-27 11:34:52 -0600

    cambios

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,7 @@
 import MultiItemCarousel from "../components/CarruselProductos";
 import CarruselHero from "../components/CarruselHero";
 import BannerPromocionales from "../components/BannerPromo";
-import Footer from "../components/Footer/Footer";
-
+import Footer from '../components/footer'
 const Home = () => {
   return (
     <>
