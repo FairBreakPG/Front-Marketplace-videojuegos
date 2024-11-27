607ec8ab9ec8b2607b6d51056500d7e57a95b2a1
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-11-27 02:02:19 -0600

    pruebas con servicio de render

--- src/config/apiconfig.js
+++ src/config/apiconfig.js
@@ -1,8 +1,10 @@
 export const URLBASE = 'http://localhost:3000'; 
 
+export const URLBASEPROD = 'https://back-marketplace-videojuegos.onrender.com';
+
 export const ENDPOINT = {
-  login: `${URLBASE}/login`,
-  usuarios: `${URLBASE}/usuarios`,
+  login: `${URLBASEPROD}/login`,
+  usuarios: `${URLBASEPROD}/usuarios`,
   perfilusuario: (id) => `${URLBASE}/usuarios/${id}`,
   productos: `${URLBASE}/productos`, 
   crearProducto: `${URLBASE}/productos`, 
