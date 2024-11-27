63dd7d96b051bec5bff3f622afbb59f8ea731c1d
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-11-27 10:18:41 -0600

    cambios

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Front-Marketplace-videojuegos/',
+    base: "/Front-Marketplace-videojuegos/",
   plugins: [react()],
   
 })
