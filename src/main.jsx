78815a346fc2dec771e2b1a0af2208bf153e5603
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-12-08 01:28:14 -0600

    fix import main

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./index.css";
-import UsuarioProvider  from '../../context/AuthProvider';
+import AuthProvider  from './context/AuthProvider';
 import ProductoProvider from "./context/ProductoProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <UsuarioProvider>
+    <AuthProvider>
       <ProductoProvider>
         <App />
       </ProductoProvider>
-      </UsuarioProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
